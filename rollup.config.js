bc7e28ec9d4a0828b42c79f898de6d08431c83ac
Sameera Chathuranga <abeywickrama.sam@gmail.com>
2019-07-31 14:28:12 +0530

    feat: 🎸 enable css modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 
 const name = "Name";
@@ -31,6 +32,9 @@ export default {
   plugins: [
     peerDepsExternal(),
     resolve(),
+    postcss({
+      modules: true
+    }),
     babel({
       exclude: "node_modules/**"
     }),
