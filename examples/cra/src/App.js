0642318619e516aeac20705f8302b6b0610ce4a7
Sameera Chathuranga <abeywickrama.sam@gmail.com>
2019-07-31 17:28:15 +0530

    feat: 🎸 add example for cra apps

---
+++ examples/cra/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MyComponent from "react-component-starter";
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
